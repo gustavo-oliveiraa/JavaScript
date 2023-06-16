e59fabb3ae53569c06a684c3cbbc5a5289078718
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-06-16 17:26:02 -0300

    Add files via upload

---
+++ comentarios.js
@@ -0,0 +1,8 @@
+// o codigo abaixo escreve alguma mensagem no devtools
+console.log('Bem vindos ao Starter')
+
+// use o "//"" para fazer comentarios em linhas
+
+/*
+  use o para fazer comnentários multi linha
+*/ 
---
+++ scripts.js
@@ -0,0 +1,15 @@
+/*
+  String 
+
+  * Cadeia de caracteres 
+
+  "" // aspas duplas
+  '' // aspas simples
+  `` // template literals ou template strings
+
+  as crases permitem a gente fazer multi linha
+
+  se usar um para o texto caso queira que tenha outra dentro tem que ser de outro modelo
+*/ 
+
+console.log('Mayk')
