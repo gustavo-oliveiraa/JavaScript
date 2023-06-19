f129f72cff325bed0af27c386ed36cd15c83b399
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-06-19 11:18:29 -0300

    Add files via upload

---
+++ dinamicos.js
@@ -0,0 +1,7 @@
+ let clima = true // boolean
+ clima = "" // string
+
+ // para ver o tipo de uma variavel é so usar "typeof"
+ console.log(typeof clima)
+
+ // será usadae mais "let" e "const", por ser uma linguagem mais moderna
