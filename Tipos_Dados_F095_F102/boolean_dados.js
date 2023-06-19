00bffaf613eb7e470f9b9a9fd4fe467d8da93132
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-06-18 21:15:04 -0300

    Add files via upload

---
+++ Tipos_Dados_F095_F102/boolean_dados.js
@@ -0,0 +1,10 @@
+/*
+  #F098 - Boolean
+  
+    * somente 2 valores
+
+    true // verdadeiro
+    false // falso
+*/
+console.log(true)
+console.log(false)
---
+++ Tipos_Dados_F095_F102/tipos_dados.js
@@ -0,0 +1,41 @@
+/*
+#F102 - Conclusão
+
+  # Tipos de dados
+
+  Conforme o ECMAScript standard temos 9 tipos de dados:
+    ECMAScript = orgão que padroniza o java script  /  standard = padrão
+
+  * Data types
+        * Primitive / Primitive value
+        * Structural
+        * Structural Primitive
+         
+  ##  Primitivos
+
+  //  dados primitivos: ñ é um objeto e seu valor é único e imutável!
+  
+  * String
+  * Number
+  * Boolean // true or false / criar caminhos
+  * undefined // ve bastante em termos de erros / criar erros
+  * Symbol
+  * BigInt
+
+  ## Estruturais
+
+* Object  // todo objeto tem alguma funcionalidade e atributo
+    * Array
+    * Map
+    * Set
+    * Date
+    * ...  
+* Function  // veremos mais na frente
+
+  ## Primitivo Estrutural / Structural Root Primitive
+
+  * null
+
+  // ao mesmo tempo ele é primitivo e estrutural
+
+*/
