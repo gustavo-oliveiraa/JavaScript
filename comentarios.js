e75c882fac3fe0c9a034ca6cfb8297be8c5bfc66
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-06-18 21:17:16 -0300

    Delete comentarios.js

--- comentarios.js
+++
@@ -1,8 +0,0 @@
-// o codigo abaixo escreve alguma mensagem no devtools
-console.log('Bem vindos ao Starter')
-
-// use o "//"" para fazer comentarios em linhas
-
-/*
-  use o para fazer comnentários multi linha
-*/ 
