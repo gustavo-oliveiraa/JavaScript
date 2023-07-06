ac68e30e80dd1c9aeee98eb351df041df01417de
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-06 14:45:50 -0300

    Update and rename dinamicos.js to F104_dinamicos.js

--- dinamicos.js
+++
@@ -1,7 +0,0 @@
- let clima = true // boolean
- clima = "" // string
-
- // para ver o tipo de uma variavel é so usar "typeof"
- console.log(typeof clima)
-
- // será usadae mais "let" e "const", por ser uma linguagem mais moderna
