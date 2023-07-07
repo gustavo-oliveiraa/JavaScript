69f1c1dc8a238a935707489f3961ad4724ae9200
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-07 14:30:16 -0300

    Add files via upload

---
+++ F095_F102_Tipos_Dados/F099_undefined_vs_null.js
@@ -0,0 +1,14 @@
+/* 
+  F099
+  * undefined
+      * indefinido
+       
+  * null
+       * nulo
+       * objeto que não possui nada dentro
+       * diferente de indefinido           
+*/
+console.log(undefined)  // é algo que não vai existir 
+console.log(null) //algo que existe mas não tem nada dentro dele
+
+console.log(null === undefined)
---
+++ F095_F102_Tipos_Dados/F100_object_dados.js
@@ -0,0 +1,18 @@
+/*
+  #F100
+  * Object
+      * Objeto
+      * Propriedades / Atributos
+      * Funcionalidades / Métodos
+  
+  { propriedade: "valor" } // assim que se escreve um obj em JS
+
+  object = dado estrutural --> cria uma estrutura
+*/
+console.log({
+  name: "Mayk", // propriedade-atributos
+  idade: 36,
+  andar: function() { // funcionalidade-metodo
+    console.log('andar')
+  }
+})
