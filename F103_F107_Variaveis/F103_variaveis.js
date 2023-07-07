5ca2c763cb9750b14563dd73754a081a39a430b1
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-07 14:31:51 -0300

    Add files via upload

---
+++ F103_F107_Variaveis/F103_variaveis.js
@@ -0,0 +1,23 @@
+/*
+*  var
+*  declaração
+
+var clima = "Quente"
+clima = "Frio"
+
+console.log(clima)
+*/
+
+/*
+*  let
+let clima = "Quente"
+clima = "Frio"
+
+console.log(clima)
+*/
+
+// const --> ela ñ pode mudar seu valor durante a aplicação / "constante" 
+const clima = "Quente"
+clima = "Frio"
+
+console.log(clima)
