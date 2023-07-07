872df0ec8d21f6528dcf99e07b324847a5f408d2
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-07 15:12:37 -0300

    Create F108_declaration_assignment_var.js

---
+++ F108_F113_Praticando/F108_declaration_assignment_var.js
@@ -0,0 +1,10 @@
+// variáveis e tipos de dados
+//declaração or declaration
+var name
+
+// assigment or atribuição de valores
+name = 'Mike'
+
+// que tipo de dado foi colocado na variável
+
+console.log(typeof name)
