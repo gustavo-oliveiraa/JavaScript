3c2e6c09c757450f75d79cb4340d16c401f0ff91
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-07-17 16:15:54 -0300

    Update and rename F110_concatenando_interpolando_variaveis to F110_concatenando_interpolando_variaveis.js

---
+++ F108_F113_Praticando/F110_concatenando_interpolando_variaveis.js
@@ -0,0 +1,27 @@
+// variáveis e tipos de dados
+//declaração or declaration
+var name
+
+// assigment or atribuição de valores
+name = 'Mike'
+
+// que tipo de dado foi colocado na variável
+// console.log(typeof name)
+
+//declarações conjuntas --> agrupar as declarações
+// agrupamento de declarações
+let age, isHuman
+
+age = 18
+isHuman = true
+
+// multiplos argumentos na função (com uso da ",") 
+console.log(name, age, isHuman)
+
+// escrita de texto + variáveis --> Concatenar
+
+// concatenando os valores
+console.log('O ' + name + ' tem ' + age + ' anos.')
+
+// interpolando valores com template literals or template strings
+console.log(`O ${name} tem ${age} anos.`)
