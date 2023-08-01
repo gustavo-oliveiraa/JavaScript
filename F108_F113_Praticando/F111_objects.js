d2d3fdf677d7225d2cd00f951e71d518b4f84e18
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-01 15:56:21 -0300

    Update F111_objects.js

--- F108_F113_Praticando/F111_objects.js
+++ F108_F113_Praticando/F111_objects.js
@@ -1,1 +1,16 @@
+// Object
 
+const person = {
+    name: 'Gustavo',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+// Aparecera o objeto completo
+console.log(person)
+
+// Aparecera somente o valor da propriedade (name ou age)
+console.log(person.name)
+
+console.log(`${person.name} tem ${person.age} anos`)
