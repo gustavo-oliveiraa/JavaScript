e1d160dfe7299d197d0bdde89e5913463307db32
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-03 16:09:56 -0300

    Create F113_Exercicios.js

---
+++ F108_F113_Praticando/F113_Exercicios.js
@@ -0,0 +1,14 @@
+// 1. Declare uma variável de nome weight
+
+let weight;
+
+// 2. Que tipo de dado é a variavel acima?
+
+console.log(typeof weight)
+
+/* 3. Declare uma variável e atribua valores para cada um dos dados:
+        * name: String
+        * age: Number (integer)
+        * stars: Number (float)
+        * isSubscribed: Boolean
+*/
