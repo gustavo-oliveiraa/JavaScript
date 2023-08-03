1288659f97dcb37b155de98332226bbc3b1cf7ba
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-03 15:35:03 -0300

    Create F112_arrays.js

---
+++ F108_F113_Praticando/F112_arrays.js
@@ -0,0 +1,25 @@
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 2    
+    }
+]
+
+// acessar valores dentro do Array
+console.log(animals)
+
+// acessar valores especificos dentro do Array
+console.log(animals[0])
+
+// para saber o tamanho do Array
+console.log(animals.length)
+
+// mostra o objeto
+console.log(animals[2])
+
+// mostrara a propriedade do objeto
+console.log(animals[2].name)
