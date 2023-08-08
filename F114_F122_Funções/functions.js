df3f0e0f01cfaa0ed0f2a1b894ea68e930038614
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-08 15:26:30 -0300

    Add files via upload

---
+++ F114_F122_Funções/functions.js
@@ -0,0 +1,24 @@
+// criar um aplicativo de frases motivacionais
+
+// declaration - declaração da função
+// funcion statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
+
+console.log('Fim do programa')
+
+
+
+
+
+//  A função tem motivo de explicar oq um pedaço de codigo esta fazendo
+//  E tem o motivo de repetir o codigo todas as vezes que necessario.
+//  agrupamento de codigo
