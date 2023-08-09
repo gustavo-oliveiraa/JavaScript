70d99ee9a8874e7bd2de226735b8274b6647fff8
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-09 15:40:21 -0300

    Add files via upload

---
+++ F114_F122_Funções/F120_arrow_function.js
@@ -0,0 +1,11 @@
+// arrow function   (função do tipo expression)
+
+const sayMyName = (name) => {    
+    console.log(name)
+}
+
+sayMyName('Gustavo')
+
+
+
+// Padrão:  const sayMyName = function() {}
