531339201a358b831d75793f3085a99f4e9d3e24
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-14 13:55:19 -0300

    Add files via upload

---
+++ F123_F134_Manipulando_Dados/F123_Prototype.js
@@ -0,0 +1,19 @@
+/*
+    Prototype
+
+    * prototype-based language
+    * prototype chain
+    *  __proto__
+
+*/ 
+// prototipo - aquilo que foi criado antes
+
+"Gustavo".__proto__  //  eles vão ter o proto dele e vão herdar toda a cadeia de prototipo
+
+23.0.__proto__
+
+"Gustavo".length
+
+11.0.hasOwnProperty()
+
+//  Cada um dos tipos de dados são embrulhados por um objeto que ja tem uma propriadade "__proto__" e podemos acessar todas as propriedades.
