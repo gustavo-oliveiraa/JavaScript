5c644b6f93774a2a5d412ba8c37aa1177c59e672
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-14 15:55:31 -0300

    Add files via upload

---
+++ F123_F134_Manipulando_Dados/F131_Criando_array_construtor.js
@@ -0,0 +1,20 @@
+// Manipulando Arrays
+
+
+// Criar Array com construtor 
+let myArray = new Array('a', 'b', 'c')
+    console.log(myArray)
+
+
+
+/* criar um array com as posições vazias
+    let myArray = new Array(10)
+    console.log(myArray)
+*/
+
+
+
+/* Criação Array
+    let myArray = ['a', 'b', 'c']
+    console.log(myArray)
+*/
