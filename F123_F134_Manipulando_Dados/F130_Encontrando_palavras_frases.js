6dd7fc5aac654c41373471c8bce446ccca0c2037
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-14 15:34:20 -0300

    Add files via upload

---
+++ F123_F134_Manipulando_Dados/F130_Encontrando_palavras_frases.js
@@ -0,0 +1,16 @@
+// Manipulando Strings
+
+
+// Verificar se o texto contém a palavra Amor
+
+let phrase = "Eu quero viver o Amor!"
+
+console.log(phrase.includes("Amor")) // includes = eu passo com argumento oq eu quero verificar se tem ou não
+
+
+
+/*
+    let phrase = "Eu quero viver o amor!"
+
+    console.log(phrase.toUpperCase().includes("AMOR")) //   outra forma de ver, usando o uppercase.
+*/
