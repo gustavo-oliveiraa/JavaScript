ce90f955fc1f9ec222ce1ddeaef95d49ace69236
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-15 14:45:51 -0300

    Add files via upload

---
+++ F123_F134_Manipulando_Dados/F133_Strings_para_arrays.js
@@ -0,0 +1,7 @@
+// Manipulando Arrays
+
+// Transformar uma cadeia de caracteres em elementos de um array
+
+let word = "manipulação" // cadeia de caracteres (string)
+
+console.log(Array.from(word)) // O proprio "Array" já em um objeto disponivel de maneira global, metodo from que espera como argumento uma string.
