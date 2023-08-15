3cafc0afcbb7492177809b84ab46fbc7bdfdf791
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-15 16:58:46 -0300

    Add files via upload

---
+++ F135_F148_Expressões_Operadores/F136_New.js
@@ -0,0 +1,19 @@
+/*
+    new
+    
+    * left-hand-side expression
+    * criar um novo objeto
+*/
+
+// new = é uma expressão que criar um novo objeto
+
+// o new faz o, (name) por exemplo, ser um novo objeto
+
+let name = new String('Gustavo')
+name.surName = 'Maxwel'
+let age = new Number(23)
+console.log(name)
+console.log(name.surName, age)
+
+let date = new Date('2023-08-15')
+console.log(date)
