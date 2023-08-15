197c40ebf15e61ba1e3ec1dcadce11387eeb58c6
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-15 16:19:26 -0300

    Delete mais_comandos_array.js

--- F123_F134_Manipulando_Dados/mais_comandos_array.js
+++
@@ -1,9 +0,0 @@
-let array = ['maça', 'laranja', 'uva']
-
-console.log(array)
-
-array.push('pera') // colocar um array
-array.pop() // tirar o ultimo array
-array.shift() // tirar o primeiro array
-
-console.log(array.length)
