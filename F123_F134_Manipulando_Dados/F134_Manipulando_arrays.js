3a48b2c8d2fbe664913ec4e136344c690845c4a8
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-15 16:19:52 -0300

    Update F134_Manipulando_arrays.js

--- F123_F134_Manipulando_Dados/F134_Manipulando_arrays.js
+++ F123_F134_Manipulando_Dados/F134_Manipulando_arrays.js
@@ -9,16 +9,16 @@ techs.push("nodejs")
 techs.unshift("sql")
 
 // remover do fim
-// techs.pop()
+techs.pop()
 
 // remover do começo
-// techs.shift()
+techs.shift()
 
 // pegar somente alguns elementos do array
-// console.log(techs.slice(1, 3)) = aparece ["css", "js"] // slice: cortar | o 1º argum recebe qual a posição de inicio que eu quero que ele retire, o 2º argum vai receber qual a posição final do array que eu quero (conta como elementos, no caso do array acima seria ate o 3)
+console.log(techs.slice(1, 3)) = aparece ["css", "js"] // slice: cortar | o 1º argum recebe qual a posição de inicio que eu quero que ele retire, o 2º argum vai receber qual a posição final do array que eu quero (conta como elementos, no caso do array acima seria ate o 3)
 
 // remover 1 ou mais itens em qualquer posição do array
-// techs.splice(1, 2) = aparace ["html"] // no 1º argum eu indico qual o index do array (oque eu quero tirar daqui), no 2º eu indico quantos argumentos eu quero tirar 
+techs.splice(1, 2) = aparace ["html"] // no 1º argum eu indico qual o index do array (oque eu quero tirar daqui), no 2º eu indico quantos argumentos eu quero tirar 
 
 // encontrar a posição de um elemento no array
 let index = techs.indexOf('css')    // indexOf: Qual o index de (...)
