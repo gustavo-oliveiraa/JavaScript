dd61e4c345f39dc4f70e16f18d805bc2b6e6ac10
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-16 14:51:32 -0300

    Add files via upload

---
+++ F135_F148_Expressões_Operadores/F140_Operadores_logicos.js
@@ -0,0 +1,16 @@
+// Operadores lógicos (logical operators)
+
+// - 2 valores booleanos, quando verificados,
+// resulturá em verdadeiro ou falso
+
+let pao = true
+let queijo = true
+
+// AND &&
+console.log(pao && queijo)
+
+// OR ||
+console.log(pao || queijo)
+
+// NOT !
+console.log(!pao) // Troca de valor, sempre que colocar uma ! na frente de uma variavel estou trocando o valor dela
