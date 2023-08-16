01bbafc684eacae4dc2e5af514dc20bb3697fcaf
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-16 14:21:06 -0300

    Add files via upload

---
+++ F135_F148_Expressões_Operadores/F138_Operadores_aritmeticos.js
@@ -0,0 +1,36 @@
+// Operadores Aritiméticos
+
+// multiplicação *
+console.log(3 * 5)
+
+// divisão /
+console.log(12 / 2)
+
+// soma +
+console.log(34 + 67)
+
+// subtração -
+console.log(34 - 23)
+
+// resto da divisão %
+let remainder // resto
+remainder = 11 % 9
+console.log(remainder)
+
+// incremento ++
+let increment = 0
+increment++
+console.log(increment)
+
+console.log(increment++) // se o incremento for depois o incremento so occore apos 
+console.log(++increment) // se o incremento for antes o incremento occore antes  
+
+// decremento --
+let decrement = 0
+decrement--
+console.log(decrement)
+
+console.log(--decrement)
+
+// exponencial **
+console.log(3 ** 3) // 3 elevado a 3
