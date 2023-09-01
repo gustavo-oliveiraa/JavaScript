3755aaebe681b92bef61ffc8c538716ead655ae0
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-09-01 14:57:36 -0300

    Update F150_If_Else.js

--- F149_F152_Condicionais_Controle_Fluxo/F150_If_Else.js
+++ F149_F152_Condicionais_Controle_Fluxo/F150_If_Else.js
@@ -1,1 +1,25 @@
+// if...else
 
+let temperature = 37
+
+if(temperature >= 37.5) {
+    console.log('Febre alta')
+} else if(temperature < 37.5 && temperature >= 37){
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+
+// transformar as condicionais em variaveis que tenham relevancia, que indiquem oq estão fazendo
+
+let temperature = 39
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+
+if(highTemperature) {
+    console.log('Febre alta')
+} else if(mediumTemperature){
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
